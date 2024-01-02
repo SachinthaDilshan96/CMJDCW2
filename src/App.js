17924482bc5bf9eea0203f1a0ed4c7588307eae8
sachinthadilshan <sachinthadilshan96@gmail.com>
2024-01-02 19:48:38 +0530

    top navbar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Header from "./pages/components/Header";
+import Footer from "./pages/components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route index element={<HomePage/>}/>
+            <Route path={"/signup"} element={<SignUpPage/>}/>
+            <Route path={"/login"} element={<LoginPage/>}/>
+          </Routes>
+        <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import "./CSS/HomePage.css"
+
+const HomePage =()=>{
+    return(
+        <div className={"main-container"}>
+dhdhdhd
+        </div>
+    )
+}
+
+export default HomePage;
