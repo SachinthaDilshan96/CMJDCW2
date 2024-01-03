350535c8fb2b1b12d907c6a5f09cd74531f2e346
sachinthadilshan <sachinthadilshan96@gmail.com>
2024-01-03 19:34:10 +0530

    home done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,23 @@
 import "./CSS/HomePage.css"
+import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
 
 const HomePage =()=>{
+    const navigate = useNavigate();
+    const changeRoute=()=>{
+        navigate("/login")
+    }
     return(
-        <div className={"main-container"}>
-dhdhdhd
+        <div className={"main-container container"}>
+            <div className={"inner-container row"}>
+                <div className={"col-sm-12 text-center"}>
+                    <h1 className={"main-header"}>Welcome to the CMJD POS System</h1>
+                </div>
+                <div className={"col-sm-12 text-center"}>
+                    <h3>Please Login to access the system</h3>
+                    <Button onClick={changeRoute} variant={"contained"}>Login</Button>
+                </div>
+            </div>
         </div>
     )
 }
